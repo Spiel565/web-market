fbc717dbad61152155092774d98231a502302b61
Spiel565 <svstorchak2@gmail.com>
2017-03-30 14:18:38 +0300

    initial commit

--- mail.php
+++ mail.php
@@ -9,11 +9,12 @@ $zamov=$_POST['zamov'];
 $koment=$_POST['koment'];
 $zamovlennya="Кліент зробив таке замовлення:<br>".$zamov."На суму: ".$full."<br>Імя клієта: ".$name."<br>Моб.Телефон: ".$phone."<br>Ел.Пошта: ".$mail."<br>Місто доставки(+30 грн): ".$street."<br>Номер відділення: ".$street2."<br>Коментар до замовлення: ".$koment."<br>";
 $adminmail='support@ps-store.com';
+$email='svstorchak2@gmail.com';
 $subject='Нове замовлення в Інтернет Магазині';
 $nameshop='PS Store';
 $headers="MIME-Version:1.0\r\n";
 $headers.="Content-type:text/html; charset=utf-8\r\n";
 $headers.="From:".$nameshop."<".$adminmail.">\r\n";
-$ok=mail($adminmail,$subject,$zamovlennya,$headers);
+$ok=mail($email,$subject,$zamovlennya,$headers);
 echo $ok;
 ?>
